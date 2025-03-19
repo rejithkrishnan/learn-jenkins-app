0e59adf691707bdbbf1f2151cb8854fd4c77ec04
rejith <rejith.uck@gmail.com>
2025-03-20 00:27:03 +0530

    added node in test stage also

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('renders learn Jenkins', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn Jenkins/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
 test('renders learn Jenkins link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn Jenkins/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
